935b148c9b3dcfff7ccea79d9b90ab40788dd44c
Adrian Tull <92965051+Ayjrin@users.noreply.github.com>
2026-05-14 21:29:07 -0700

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # how do i not use an extra variable and get rid of all these returns?
+        new_t = ""
+        if len(s) == 0:
+            return True
+        elif len(t) == 0 and len(s) != 0:
+            return False
+        if len(t) < len(s):
+            return False 
+        for i in range(len(s)):
+            while t[i] != s[i]:
+                if len(t) < len(s):
+                    return False 
+                t = t[1:]
+                if len(t) == 0 and len(s) - i != 1:
+                    return False # if s is longer than t and also s has the subset, then false. then. its a superset.
+            if t[i] == s[i]:
+                new_t += t[i]
+        if new_t == s:
+            return True
+        else: return False
