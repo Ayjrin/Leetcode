25ee3d6ab01539b2f518ba11d3b957092ca9e95f
Adrian Tull <92965051+Ayjrin@users.noreply.github.com>
2026-05-29 11:16:02 -0700

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        grace = True
+        while s[l] == s[r]:
+            if r - l <2:
+                return True
+            else:
+                l += 1
+                r -= 1
+                if grace == True and s[l] != s[r]:
+                    if s[l+1] == s[r]:
+                        l+=1
+                        grace = False
+                        continue
+                    elif s[r-1] == s[l]:
+                        r-=1
+                        grace = False
+                        continue
+                    else:
+                        return False
+
+        return False
